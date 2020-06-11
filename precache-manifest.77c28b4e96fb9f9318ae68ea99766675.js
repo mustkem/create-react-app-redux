b0cc621e052a4c5c4219be01a2cd8c395ddc5f4f
mustkeem <mo.mustkem@gmail.com>
2020-06-12 04:14:10 +0530

    Updates

---
+++ precache-manifest.77c28b4e96fb9f9318ae68ea99766675.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c487661d75d99a4c7b5a7a807b5a0b5",
+    "url": "/react-workshop-diagnal/index.html"
+  },
+  {
+    "revision": "687b50a975f39fa22da3",
+    "url": "/react-workshop-diagnal/static/css/main.13d0ec52.chunk.css"
+  },
+  {
+    "revision": "f6157560a4719f79a715",
+    "url": "/react-workshop-diagnal/static/js/2.57b95db1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-workshop-diagnal/static/js/2.57b95db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687b50a975f39fa22da3",
+    "url": "/react-workshop-diagnal/static/js/main.8a189296.chunk.js"
+  },
+  {
+    "revision": "274f010fa4ce1d22c99b",
+    "url": "/react-workshop-diagnal/static/js/runtime-main.f0187560.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/react-workshop-diagnal/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/react-workshop-diagnal/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/react-workshop-diagnal/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/react-workshop-diagnal/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/react-workshop-diagnal/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/react-workshop-diagnal/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/react-workshop-diagnal/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/react-workshop-diagnal/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/react-workshop-diagnal/static/media/poster9.65d6d500.jpg"
+  }
+]);
